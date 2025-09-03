73cf9699b34bd9ee60e1807fd877a45bfc818038
ismaeli34 <ronneyismael@gmail.com>
2025-09-03 16:56:06 +0200

    added church staff members

--- src/app/about/church-staff/church-staff.component.ts
+++ src/app/about/church-staff/church-staff.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-church-staff',
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './church-staff.component.html',
   styleUrl: './church-staff.component.scss'
 })
 export class ChurchStaffComponent {
+ // defaultPhoto = 'assets/images/avatars/user-01.jpg'; // Add a placeholder image here
+  staffMembers = [
+    {
+      name: 'Prince Edos',
+      title: 'Pastor, Elder, Administrative Team',
+      email: 'pastor@mcci.de',
+      photo: 'pastor-01.jpg'
+    },
+    {
+      name: 'Prince Edos',
+      title: 'Pastor, Elder, Administrative Team',
+      email: 'pastor@mcci.de',
+      photo: 'pastor-03.jpg'
+    },
+    {
+      name: 'Prince Edos',
+      title: 'Pastor, Elder, Administrative Team',
+      email: 'pastor@mcci.de',
+      photo: 'pastor-01.jpg'
+
+    },
+    {
+      name: 'Prince Edos',
+      title: 'Pastor, Elder, Administrative Team',
+      email: 'pastor@mcci.de',
+      photo: 'pastor-03.jpg'
+
+    },
+  ];
+
 
 }
--- src/app/about/supporting/supporting.component.ts
+++ src/app/about/supporting/supporting.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-supporting',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './supporting.component.html',
   styleUrl: './supporting.component.scss'
 })
