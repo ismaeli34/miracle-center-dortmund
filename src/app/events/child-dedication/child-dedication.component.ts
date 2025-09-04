a1d18ae8e061a8edad386167cf6a6e165e3f87ae
ismaeli34 <ronneyismael@gmail.com>
2025-09-04 11:32:26 +0200

    holy communion, midnight service, membership implemented

--- src/app/events/child-dedication/child-dedication.component.ts
+++ src/app/events/child-dedication/child-dedication.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from "@angular/material/expansion";
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-child-dedication',
-  imports: [],
+  imports: [
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle,
+    MatIconModule,
+    MatAccordion
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './child-dedication.component.html',
   styleUrl: './child-dedication.component.scss'
 })
 export class ChildDedicationComponent {
+  readonly panelOpenState = signal(false);
+
 
 }
--- src/app/events/membership-class/membership-class.component.ts
+++ src/app/events/membership-class/membership-class.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle
+} from "@angular/material/expansion";
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-membership-class',
-  imports: [],
+    imports: [
+        MatAccordion,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatExpansionPanelTitle,
+        MatIconModule
+    ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './membership-class.component.html',
   styleUrl: './membership-class.component.scss'
 })
 export class MembershipClassComponent {
+
+  readonly panelOpenState = signal(false);
+
 
 }
