8a6c44a0ef85eea87afea4479ab1d354ad7bd75a
ismaeli34 <ronneyismael@gmail.com>
2025-09-05 10:44:41 +0200

    Stop tracking environment.ts

--- src/app/environment/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  bankDetails: {
-    accountHolder: 'Miracle Center Church International e.V',
-    iban: 'DE49 0000 0000 0000 0000 00',
-    bankName: 'Commerzbank Dortmund',
-    bic:'COBADEFFXXX',
-  }
-};
